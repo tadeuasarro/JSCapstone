80bf3854e208cc8f5a60316ae85de47938314abd
tadeuasarro <tadeutas@gmail.com>
2021-01-04 17:33:12 -0300

    Add meeting enemies feature

--- src/index.js
+++ src/index.js
@@ -61,8 +61,46 @@ var WorldScene = new Phaser.Class({
     this.cameras.main.startFollow(this.player);
     this.cameras.main.roundPixels = true;
 
-  },
+    //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13]}),
+        frameRate: 10,
+        repeat: -1
+    });
 
+    // animation with key 'right'
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { frames: [1, 7, 1, 13] }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('player', { frames: [2, 8, 2, 14]}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 6, 0, 12 ] }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.physics.add.collider(this.player, obstacles);
+
+    this.spawns = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
+    for(var i = 0; i < 30; i++) {
+      var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+      var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+      // parameters are x, y, width, height
+      this.spawns.create(x, y, 20, 20);
+    }
+    this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
+
+  },
   update: function (time, delta)
   {
     this.player.body.setVelocity(0);
@@ -70,22 +108,33 @@ var WorldScene = new Phaser.Class({
     // Horizontal movement
     if (this.cursors.left.isDown)
     {
-        this.player.body.setVelocityX(-80);
+      this.player.body.setVelocityX(-80);
+      this.player.anims.play('left', true);
     }
     else if (this.cursors.right.isDown)
     {
-        this.player.body.setVelocityX(80);
+      this.player.body.setVelocityX(80);
+      this.player.anims.play('right', true);
     }
-
-    // Vertical movement
-    if (this.cursors.up.isDown)
+    else if (this.cursors.up.isDown)
     {
-        this.player.body.setVelocityY(-80);
+      this.player.body.setVelocityY(-80);
+      this.player.anims.play('up', true);
     }
     else if (this.cursors.down.isDown)
     {
-        this.player.body.setVelocityY(80);
+      this.player.body.setVelocityY(80);
+      this.player.anims.play('down', true);
     }
+    else
+    {
+        this.player.anims.stop();
+    }
+  },
+  onMeetEnemy: function(player, zone){
+    zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+    zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+    this.cameras.main.flash(150);
   }
 });
 
@@ -99,7 +148,8 @@ var config = {
   physics: {
       default: 'arcade',
       arcade: {
-          gravity: { y: 0 }
+          gravity: { y: 0 },
+          debug: true
       }
   },
   scene: [
