9f4eb35ecc8c66ac196a1baf4abeb79b32ad9236
tadeuasarro <tadeutas@gmail.com>
2021-01-04 12:27:25 -0300

    Add webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+console.log('carajo')
--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,9 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 };
