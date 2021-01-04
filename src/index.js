199684eee9ceaa6c8cdf82e058ce1c04dcde4031
tadeuasarro <tadeutas@gmail.com>
2021-01-04 16:50:38 -0300

    Add player movement

--- src/index.js
+++ src/index.js
@@ -48,6 +48,39 @@ var WorldScene = new Phaser.Class({
     var grass = map.createStaticLayer('Grass', tiles, 0, 0);
     var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
     obstacles.setCollisionByExclusion([-1]);
+
+    this.player = this.physics.add.sprite(50, 100, 'player', 6);
+
+    this.physics.world.bounds.width = map.widthInPixels;
+    this.physics.world.bounds.height = map.heightInPixels;
+    this.player.setCollideWorldBounds(true);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  },
+
+  update: function (time, delta)
+  {
+    this.player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown)
+    {
+        this.player.body.setVelocityX(-80);
+    }
+    else if (this.cursors.right.isDown)
+    {
+        this.player.body.setVelocityX(80);
+    }
+
+    // Vertical movement
+    if (this.cursors.up.isDown)
+    {
+        this.player.body.setVelocityY(-80);
+    }
+    else if (this.cursors.down.isDown)
+    {
+        this.player.body.setVelocityY(80);
+    }
   }
 });
 
