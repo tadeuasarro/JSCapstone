a016cebe739194780ae40c75cd0de250ccae38fc
tadeuasarro <tadeutas@gmail.com>
2021-01-04 14:45:41 -0300

    Set Phaser to run with npm

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
-import createGame from './create_game';
-import fetchGame from './fetch_game';
-import createScore from './create_score';
+import Phaser from 'phaser'
 
-const index = () => {
+function preload() {
+  this.load.setBaseURL('http://labs.phaser.io')
+  this.load.image('sky', 'assets/skies/space3.png')
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+  this.load.image('red', 'assets/particles/red.png')
+}
 
-  async function callback() {
-    const result = await fetchGame();
-    return result;
-  }
+function create() {
+  this.add.image(400, 300, 'sky')
 
-  callback();
+  const particles = this.add.particles('red')
 
-};
+  const emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD',
+  })
 
-index();
+  const logo = this.physics.add.image(400, 100, 'logo')
+
+  logo.setVelocity(100, 200)
+  logo.setBounce(1, 1)
+  logo.setCollideWorldBounds(true)
+
+  emitter.startFollow(logo)
+}
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+  },
+}
+
+const game = new Phaser.Game(config)
