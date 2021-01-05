29689daa2bc1d53bd3eb41fe69984646e7fbd684
tadeuasarro <tadeutas@gmail.com>
2021-01-05 13:19:31 -0300

    Add player movement

--- src/world/world.js
+++ src/world/world.js
@@ -12,7 +12,7 @@ const world = () => {
     preload: function (){
       this.load.image('tiles', 'assets/map/spritesheet.png');
       this.load.tilemapTiledJSON('map', 'assets/map/map.json');
-      this.load.spritesheet('player', 'assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+      this.load.spritesheet('player', 'assets/heroes.png', { frameWidth: 16, frameHeight: 16 });
     },
 
     create: function (){
@@ -35,6 +35,25 @@ const world = () => {
       var grass = map.createStaticLayer('Grass', tiles, 0, 0);
       var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
       obstacles.setCollisionByExclusion([-1]);
+      this.player = this.physics.add.sprite(50, 100, 'player', 0);
+      this.physics.world.bounds.width = map.widthInPixels;
+      this.physics.world.bounds.height = map.heightInPixels;
+      this.player.setCollideWorldBounds(true);
+      this.cursors = this.input.keyboard.createCursorKeys();
+    },
+    update: function(time, delta){
+      this.player.body.setVelocity(0);
+      if (this.cursors.left.isDown){
+        this.player.body.setVelocityX(-80);
+      }else if (this.cursors.right.isDown){
+        this.player.body.setVelocityX(80);
+      }
+
+      if (this.cursors.up.isDown){
+        this.player.body.setVelocityY(-80);
+      }else if (this.cursors.down.isDown){
+        this.player.body.setVelocityY(80);
+      }
     }
   });
 
