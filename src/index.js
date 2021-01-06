b3d8b912ae0b39cede3a7ffef16fd313dbb3c598
tadeuasarro <tadeutas@gmail.com>
2021-01-06 20:14:04 -0300

    Fix some localstorage features

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import image from './bg.png';
 
 const display = document.getElementById('gameStart');
 display.addEventListener('click', () => {
+
+  localStorage.setItem('player', document.getElementById('playerName').value);
+
   document.body.innerHTML = '';
+
   gameWorld();
 })
 
