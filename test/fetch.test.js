cff943e6658c383134ba7461efc5f65fecd0824f
tadeuasarro <tadeutas@gmail.com>
2021-01-10 10:08:33 -0300

    Fix some misspelling

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -2,11 +2,11 @@ import fetchGame from '../src/scoreboard/fetch_game';
 
 describe('Test get method for retrieve scores', () => {
   describe('Test score board length', () => {
-    it('checks if the score board has a length = 0', () => {
+    it('checks if the response is truthy', () => {
       const board = fetchGame();
       expect(board).toBeTruthy();
     });
-    it('checks if the score board has a length = 0', () => {
+    it('checks if the response is a promise', () => {
       const board = fetchGame();
       expect(board).toBeInstanceOf(Promise);
     });
